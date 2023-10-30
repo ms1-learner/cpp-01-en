23645dbe59547ac222516d713f83c5026a90e098
Alexander Bolinsky <alexander.bolinsky@woven.toyota>
2023-10-30 13:50:45 +0545

    Update run.bat

--- 01_introduction_to_cpp/01_challenge/autograder/run.bat
+++ 01_introduction_to_cpp/01_challenge/autograder/run.bat
@@ -1,3 +1,7 @@
-cmake -S autograder -B autograder\build
+@echo off
+
+cmake -S autograder -B autograder\build -G "MinGW Makefiles"
 cmake --build autograder\build
 autograder\build\Tests.exe
+
+@echo on
