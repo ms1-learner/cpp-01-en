f1ebcc5c53303244abaf0a43b172853a66f5ac39
Alexander Bolinsky <alexander.bolinsky@woven.toyota>
2023-10-30 13:51:18 +0545

    Update rename.bat

--- 01_introduction_to_cpp/01_challenge/autograder/rename.bat
+++ 01_introduction_to_cpp/01_challenge/autograder/rename.bat
@@ -3,17 +3,13 @@ setlocal
 
 set empty=
 
-g++ -std=c++17 -o autograder\main main.cpp 2> nul
+g++ -std=c++17 -o autograder\main main.cpp >nul 2>&1
 if errorlevel 1 (
     set empty=empty
-) else (
-    del autograder\main.exe
 )
 
 g++ -std=c++17 -o autograder\rename_main autograder\rename_main.cpp
 
 autograder\rename_main main.cpp autograder\gen\dummy_main.cpp %empty%
-
-del autograder\rename_main.exe
 
 endlocal
