e5ef7a22af9b9cf2d205a2b55164c1d2be62590e
Alexander Bolinsky <alexander.bolinsky@woven-planet.global>
2023-10-30 19:54:11 +0545

    unit 1 challenges

--- 01_introduction_to_cpp/02_challenge/autograder/gen/dummy_main.cpp
+++ 01_introduction_to_cpp/02_challenge/autograder/gen/dummy_main.cpp
@@ -1,19 +1,16 @@
-# Challenge 2
+#include <iostream>
 
-Fill in the missing operators to make all the boolean expressions evaluate correctly.
-
-```cpp
-int main() {
+int dummy_main() {
     // use relational operators, such as == or < to make these expressions `true`
-    bool expr1 {6 7};
-    bool expr2 {10 9};
-    bool expr3 {500 500};
+    bool expr1 {6 < 7};
+    bool expr2 {9 < 10};
+    bool expr3 {500 == 500};
 
     // use logical operators to make your own expressions
     // use each logical operator at least once
-    bool expr4 {}
-    bool expr5 {}
-    bool expr6 {}
+    bool expr4 {3 >= 4};
+    bool expr5 {1 != 2};
+    bool expr6 {4 <= 5};
 
     // Do not change these lines
     // These lines will check the how each boolean expression evaluates
@@ -25,4 +22,3 @@ int main() {
     std::cout << expr6 << std::endl;
     return 0;
 }
-```
--- 01_introduction_to_cpp/03_challenge/autograder/gen/dummy_main.cpp
+++ 01_introduction_to_cpp/03_challenge/autograder/gen/dummy_main.cpp
@@ -1,32 +1,19 @@
-# Challenge 3
-
-In this Challenge, you will practice using fundamental C++ skills and keywords.
-
-## Requirements
-
-- Copy and Paste the below code into a new `.cpp` file.
-- Read the comments in the code, and follow the instructions complete the tasks.
-- Compile the code and execute it.
-
-If the code compiles and executes without errors, the challenge is complete.
-
-```cpp
 #include <iostream>
 #include <string>
 
-int main() {
+int dummy_main() {
     // Complete everything in this program.
     // Compile and execute the program without errors.
 
     // Complete the following variables by using the correct types for the data.
-    pizzaSlices = 12;
-    pi = 3.14159f;
-    starsInTheUniverse = 1234567890LL;
-    aLetter = 'k';
-    aWord = "kaleidescope";
-    veryPrecisePi = 3.14159265358979;
-    thisIsTrue = true;
-    thisIsAlsoTrue = thisIsTrue;
+    int pizzaSlices = 12;
+    float pi = 3.14159f;
+    long long starsInTheUniverse = 1234567890LL;
+    char aLetter = 'k';
+    std::string aWord = "kaleidescope";
+    double veryPrecisePi = 3.14159265358979;
+    bool thisIsTrue = true;
+    bool thisIsAlsoTrue = thisIsTrue;
 
     // On the next three lines, declare and initialize a new variable on each line. Each variable must be a different data type.
     // variable #1 here
@@ -34,17 +21,17 @@ int main() {
     // variable #3 here
 
     // Complete these arrays by giving them the correct data type.
-    myArray[] = {1, 2, 3, 4}
-    anotherArray[] = {3.5, 1.2, 6.7}
+    int myArray[] = {1, 2, 3, 4};
+    float anotherArray[] = {3.5, 1.2, 6.7};
 
     // Make your own array on the next line. It should contain at least 5 numbers. The numbers can be any number data type, but they must all be the same data type.
 
     // This array should not use `int` because it stores big numbers. What data type is better?
-    bigNumbers[] = {500000, 700000, 800000}
+    long long bigNumbers[] = {500000, 700000, 800000};
 
     // Use static_cast to change 'sizeOfCesiumAtom' to an int. Use 'static_cast' on the line after sizeOfCesiumAtom.
-    double sizeOfCesiumAtom = 0.267
-    sizeOfCesiumAtomCast
+    double sizeOfCesiumAtom = 0.267;
+    int sizeOfCesiumAtomCast = static_cast<int>(sizeOfCesiumAtom);
 
     // Make three constant variables. You can use any data type, but each variable must be a different data type.
     // constant variable #1 here
@@ -64,4 +51,3 @@ int main() {
 
     return 0;
 }
-```
