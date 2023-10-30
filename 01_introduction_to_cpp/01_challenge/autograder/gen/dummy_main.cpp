a6cd0e0a3fc4dc13ce71af4ab711a2d1bfd64189
Alexander Bolinsky <alexander.bolinsky@woven-planet.global>
2023-10-30 16:50:39 +0545

    update autograder files

--- 01_introduction_to_cpp/01_challenge/autograder/gen/dummy_main.cpp
+++ 01_introduction_to_cpp/01_challenge/autograder/gen/dummy_main.cpp
@@ -1,14 +1,1 @@
-#include <iostream>
-
-int dummy_main() {
-    //add any missing values, data types or other code to complete the program
-
-    int myNumber = 3;
-
-    int anotherNumber = 5;
-    
-    int answer = myNumber + anotherNumber;
-
-    std::cout << answer << std::endl;
-    return 0;
-}
+int dummy_main() { return 1; }
--- 01_introduction_to_cpp/01_challenge/autograder/rename_main.cpp
+++ 01_introduction_to_cpp/01_challenge/autograder/rename_main.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <filesystem>
 
 void rename_main_function(const std::string& input_filepath, const std::string& output_filepath, bool empty = false) {
+    std::ofstream output_file(output_filepath);
+    if (!output_file.is_open()) {
+        std::cerr << "Failed to open output file." << std::endl;
+        return;
+    }
+
     std::ifstream input_file(input_filepath);
-    if (!input_file.is_open()) {
-        std::cerr << "Failed to open input file." << std::endl;
+    if (!input_file.is_open() || empty) {
+        output_file << "int dummy_main() { return 1; }";
+        output_file.close();
         return;
     }
 
@@ -18,24 +24,14 @@ void rename_main_function(const std::string& input_filepath, const std::string& 
     input_file.close();
 
     size_t pos = content.find("int main(");
+
     if (pos != std::string::npos) {
         content.replace(pos, 9, "int dummy_main(");
-    } else {
-        std::cerr << "main function not found." << std::endl;
-        return;
-    }
-
-    std::ofstream output_file(output_filepath);
-    if (!output_file.is_open()) {
-        std::cerr << "Failed to open output file." << std::endl;
-        return;
-    }
-
-    if (empty) {
-        output_file << "int dummy_main() { return 1; }";
-    } else {
         output_file << content;
+    } else {
+        output_file << "int dummy_main() { return 1; }";
     }
+
     output_file.close();
 }
 
