0528fce52c81150fdbea26328793b330f1b722c8
Alexander Bolinsky <alexander.bolinsky@woven-planet.global>
2023-12-19 18:11:30 +0900

    Fix rename_main adding return statement

--- 03_organization_and_testing/03_challenge/autograder/rename_main.cpp
+++ 03_organization_and_testing/03_challenge/autograder/rename_main.cpp
@@ -5,18 +5,6 @@
 #include <algorithm>
 #include <cctype>
 
-void add_return(std::string& content) {
-    auto it = std::find_if(content.rbegin(), content.rend(), [](char ch) {
-        return !std::isspace(ch);
-    });
-    content.erase(it.base(), content.end());
-    
-    if (!content.empty() && content.back() == '}') {
-        content.pop_back();
-        content += "\n    return 0;\n}";
-    }
-}
-
 void rename_main_function(const std::string& input_filepath, const std::string& output_filepath, bool empty = false) {
     std::filesystem::path dir = std::filesystem::path(output_filepath).parent_path();
     if (!std::filesystem::exists(dir)) {
