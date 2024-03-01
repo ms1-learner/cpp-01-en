01091dd57bb5641e991b6324db4065f8358bd505
Alexander Bolinsky <alexander.bolinsky@woven-planet.global>
2024-03-01 15:12:24 +0900

    Update challenges 04_02

---
+++ 04_memory_and_error_handling/02_challenge/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    int a = 10;
+    int b = 20;
+
+    std::cout << "Before swap: a = " << a << ", b = " << b << std::endl;
+    // TODO
+    std::cout << "After swap: a = " << a << ", b = " << b << std::endl;
+}
